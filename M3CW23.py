93814f02344a6f0d108bb6aa1db6e695c59ae286
actonj4703 <actonj4703@student.faytechcc.edu>
2025-02-06 14:28:01 -0500

    Add files via upload

---
+++ M3CW23.py
@@ -0,0 +1,43 @@
+#Joshua Acton
+#2-4-2025
+#M3CW23
+#This program will demo python code
+
+# declare varuable for first name
+
+fname = input("Enter your first name: ")
+
+
+lname = input("Enter your last name: ")
+
+age = int(input("Enter your age: "))
+
+
+num1 = float(input("Enter a number: "))
+
+print(fname,lname)
+                   
+print(lname)
+
+print(age)
+                   
+print("My first name is " + str(fname) + " and my last name is " + str(lname) + ".")
+
+print("my age is " + str(age) + ".")
+
+#print("Why am I here" + "?")
+
+#print(num1)
+
+                  
+
+##print("Joshua")
+##print("David")
+##print("Acton")
+
+
+
+
+
+
+
