b85f83c4bbca6ff796bdaede54879448d84b6d3f
actonj4703 <actonj4703@student.faytechcc.edu>
2025-02-06 14:20:17 -0500

    Add files via upload

---
+++ P1LAB1_ActonJoshua.py
@@ -0,0 +1,12 @@
+#Joshua Acton
+#2-6-2025
+#P1Lab1
+#This program will demo python code
+
+fname = input("Enter your first name: ")
+
+
+lname = input("Enter your last name: ")
+
+print("Hello, " + str(fname) + " " + str(lname) + "! Welcome to CTI-110")
+#print("Hello,",fname, str(lname) + "! Welcome to CTI-110")
