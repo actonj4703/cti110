3ca2ec8843ed24b6c1ca41b6a518eb9f54a8dcb9
actonj4703 <actonj4703@student.faytechcc.edu>
2025-02-11 14:07:48 -0500

    Add files via upload

---
+++ P1HW1_ActonJoshua.py
@@ -0,0 +1,35 @@
+#Joshua Acton
+#2-11-2025
+#P1HW1
+#This program will demo python code
+print()
+print("-----Calculating Exponents-----")
+print()
+print()
+
+base= int(input("Enter an integer as the base value:"))
+
+exponent= int(input("Enter an integer as the exponent:"))
+print()
+print()
+value=base**exponent
+print(base, "raised to the power of",exponent, "is",value,"!!")
+print()
+print()
+
+
+print("-----Addition and Subtraction-----")
+print()
+print()
+starting= int(input("Enter a starting integer:"))
+add= int(input("Enter an integer to add:"))
+subtract= int(input("Enter an integer to subtract:"))
+answer= starting+add-subtract
+print()
+print(starting ,"+", add ,"-", subtract, "is equal to", answer)
+
+
+
+input("Press any key to continue")
+
+
