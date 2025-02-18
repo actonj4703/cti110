718da52f019d28aa0e6cc496f69d807c77b177c5
actonj4703 <actonj4703@student.faytechcc.edu>
2025-02-18 14:30:05 -0500

    Add files via upload

---
+++ P2LAB1_ActonJoshua.py
@@ -0,0 +1,28 @@
+#Joshua Acton
+#2-18-2025
+#P2Lab1
+#import math module to use the constant, math.pi
+import math
+
+radius=float(input("What is the radius of the circle? "))
+
+diameter=radius*2
+
+
+circumference=2*radius*math.pi
+
+area=math.pi*math.pow(radius,2)
+
+
+
+
+print()
+print()
+print(f'The daimeter of the circle is {diameter:.1f}\n')
+print()
+print()
+print(f'The circumference of the circle is {circumference:.2f}\n')
+print()
+print()
+print(f'The area of the circle is {area:.3f}\n')
+
