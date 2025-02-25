22de532ca3e767873f1e31b3e47b2aeb14273496
actonj4703 <actonj4703@student.faytechcc.edu>
2025-02-25 14:51:14 -0500

    Add files via upload

---
+++ Joshua Acton programming stuff/alignment_practice.py
@@ -0,0 +1,16 @@
+first = "Joshua"
+middle = "David"
+last = "Acton"
+name1 = "Alice"
+age = 24
+
+# Left alignment with width 10
+print(f"{first:<10} is {age} years old.")
+
+
+# Left alignment with width 10
+print(f"{middle:<10} is {age} years old.")
+
+
+# Left alignment with width 10
+print(f"{last:<10} is {age} years old.")
