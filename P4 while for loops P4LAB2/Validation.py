e2a9a27e7aef2a8c18278063419c8966f84be079
actonj4703 <actonj4703@student.faytechcc.edu>
2025-03-25 14:18:59 -0400

    Add files via upload

---
+++ P4 while for loops P4LAB2/Validation.py
@@ -0,0 +1,21 @@
+keep_going = 'yes'
+
+while keep_going.lower() == 'yes':
+    
+    num = int(input("Enter an integer: "))
+    while num < 0 or num > 12:
+       print("Invalid Entry!")
+       print("Integer must be greater than or equal to 0 and less than 13")
+       num = int(input("Enter an integer: "))
+    
+    
+    print("\n")
+    if(num >= 0):
+        for i in range(1,12+1):
+         print(f'{num} * {i} = {num * i}')
+    else:
+        print("This program does not handle negative values")
+        print("\n")
+    keep_going = input("Do you want to run the program again? Enter yes or no: ")
+print("\n")
+print("Exiting the program...")
